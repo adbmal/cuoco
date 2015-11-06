a53795aa4d3ae0f13bd2d05f6f0c670104c86166
game-netease <1wei@live.com>
2015-11-06 09:21:09 +0800

    what

--- cuoco.c
+++ cuoco.c
@@ -97,6 +97,7 @@ int main(int argc, char *argv[])
   printf("Hi, main!\n");
   printf("routine create %p!\n", cuoco_create((cuoco_ctx_pfn_t)sayhi, (void *)20));
   printf("routine create %p!\n", cuoco_create((cuoco_ctx_pfn_t)saybye, (void *)10));
+  printf("routine create %p!\n", cuoco_create((cuoco_ctx_pfn_t)saybye, (void *)10));
 
   cuoco_start();
 
